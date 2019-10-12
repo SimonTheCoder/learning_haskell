dba05e5f42de457d2329d8513de5d2849bb03be6
Simon Shi <lordsimon.shi@gmail.com>
2019-10-12 10:36:46 +0800

    Update playground.hs

--- playground.hs
+++ playground.hs
@@ -1,6 +1,6 @@
 -- While learning, we will need overwrite the functions in Prelude or other libs.
 -- Add hiding below if needed.
-import Prelude hiding (sum, (++), last)
+import Prelude hiding (sum, (++))
 
 -- A function calc sum of a list. 
 sum [] = 0
@@ -13,7 +13,11 @@ sum (x:xs) = x + sum(xs)
 -- sqrt 1 to n, then sum
 ss n = foldr (\x-> \y->x+y) 0 (map (\x->x*x) [1..n])
 
--- last
-last_l x '\0' = x
-last_l x y = y
-last str = foldr last_l '\0' str 
+-- last, for string only
+my_last_l x '\0' = x
+my_last_l x y = y
+my_last str = foldr my_last_l '\0' str 
+
+-- init, for string only
+my_init_f x "\0"="";my_init_f x y=x:y
+my_init str = foldr init_f "\0" str
